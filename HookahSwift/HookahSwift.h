88316c2c7cb9f29c3b93e2ca3d811f4465ec3b89
hussien.sh <hoseen.sh.92@gmail.com>
2021-03-30 14:50:31 +0300

    Initial Commit

---
+++ HookahSwift/HookahSwift.h
@@ -0,0 +1,18 @@
+//
+//  HookahSwift.h
+//  HookahSwift
+//
+//  Created by Hussein Alshlash on 30.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HookahSwift.
+FOUNDATION_EXPORT double HookahSwiftVersionNumber;
+
+//! Project version string for HookahSwift.
+FOUNDATION_EXPORT const unsigned char HookahSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HookahSwift/PublicHeader.h>
+
+
